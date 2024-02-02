ef6e4750b06f2df8d86dad5461bbbe74dcf5b8eb
Ney Hiwerson <hiwerson@gmail.com>
2024-02-01 22:18:35 -0300

    filtro concluido e ajeitando a responsividade dos cards

--- assets/scripts/estoque.js
+++ assets/scripts/estoque.js
@@ -1,6 +1,34 @@
 $(document).ready(function(){
-    $(".dropdown-item").click(function(){
+    $(".ddpItemTipoDeVeiculo").click(function(){
         var selectedOption = $(this).data("value");
         $("#ddbTipoDeVeiculo").html(selectedOption);
     });
 });
+
+$(document).ready(function(){
+    $(".ddpItemMarcaDoVeiculo").click(function(){
+        var selectedOption = $(this).data("value");
+        $("#ddbMarca").html(selectedOption);
+    });
+});
+
+function formatarMoeda(input) {
+    var valor = input.value.replace(/\D/g, '');
+    var options = { style: 'currency', currency: 'BRL' };
+    input.value = new Intl.NumberFormat('pt-BR', options).format(valor / 100);
+}
+
+$(document).ready(function(){
+    $(".ddpItemCor").click(function(){
+        var selectedOption = $(this).data("value");
+        $("#ddbCor").html(selectedOption);
+    });
+});
+
+// Função para limpar os filtros
+function limparFiltros() {
+    // Limpe os valores dos campos de filtro aqui
+    document.getElementById('ddbTipoDeVeiculo').innerText = 'Todos';
+    // ... (limpe os outros campos conforme necessário)
+}
+
